2fb1e5e6c0fea8ac0999f9870eb236b8aa6a58ac
Virgínia Letícia Afonso <vlafonso@hotmail.com>
2026-08-22 15:05:42 -0300

    feat: add backend basico com fastapi

---
+++ backend/src/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Olá, Sistemas Distribuídos!"}
+
+
+@app.get("/hello/{name}")
+def hello(name: str):
+    return {"message": f"Olá, {name}!"}
